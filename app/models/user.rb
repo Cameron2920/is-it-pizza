47fcc8cf59fd9cdddf2754317038be403d72f8be
Cameron2920 <caramsay@uwaterloo.ca>
2017-05-22 00:17:18 -0400

    Just a few more important changes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,13 @@ class User < ActiveRecord::Base
 
   has_many :questionable_pizzas
 
-  validates_presence_of :first_name, :last_name
+  validates_presence_of :first_name, :last_name, :middle_name
+
+  before_validation do
+    if self.middle_name.nil?
+      self.middle_name = User.guess_middle_name
+    end
+  end
 
   def can_ask_cam
     allowed_pending_pizza_count = 1
@@ -15,5 +21,12 @@ class User < ActiveRecord::Base
       allowed_pending_pizza_count = [[((accepted_pizza_count.to_f / total_pizza_count.to_f) * QuestionablePizza::MAX_NUMBER_OF_PENDING_PIZZAS).ceil, accepted_pizza_count].min, 1].max
     end
     self.questionable_pizzas.where(:is_it_pizza => QuestionablePizza.is_it_pizzas[:waiting_on_cam]).count < allowed_pending_pizza_count
+  end
+
+  def self.guess_middle_name
+    possible_middle_names = ["the original dubois", "jeanboi's second cousin", "肮脏的狗",
+                             "definitely doesn't want to see the 3rd reich return", "probably not a rapist",
+                             "no middle name required", "pomagranite buttcheeks"]
+    possible_middle_names.sample
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522002253) do
+ActiveRecord::Schema.define(version: 20170522040801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170522002253) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "middle_name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
