5d6e1368ad85a19fc746a4241f2af99ed1341cc5
Cameron2920 <caramsay@uwaterloo.ca>
2022-10-10 18:07:37 -0400

    Updated docker production commands.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
+SSH_URL="root@178.128.135.203"
+docker login || { echo "Failed to login to docker"; exit 1; }
 docker build -t camcamcam/is-it-pizza -f docker/Dockerfile . || { echo "Failed to build docker image"; exit 1; }
 docker push camcamcam/is-it-pizza || { echo "Failed to push docker image"; exit 1; }
-# upload docker directory and start-production-server command to remote host
-# run start-production-server on remote
+rsync -a -e "ssh -i ~/.ssh/server_keys/is-it-pizza" docker $SSH_URL:/var/is-it-pizza/ || { echo "Failed to upload files"; exit 1; }
+rsync -a -e "ssh -i ~/.ssh/server_keys/is-it-pizza" start-production-server.sh $SSH_URL:/var/is-it-pizza/ || { echo "Failed to upload files"; exit 1; }
+rsync -a -e "ssh -i ~/.ssh/server_keys/is-it-pizza" app.env $SSH_URL:/var/is-it-pizza/ || { echo "Failed to upload files"; exit 1; }
+rsync -a -e "ssh -i ~/.ssh/server_keys/is-it-pizza" database.env $SSH_URL:/var/is-it-pizza/ || { echo "Failed to upload files"; exit 1; }
+ssh -i ~/.ssh/server_keys/is-it-pizza -t $SSH_URL "cd /var/is-it-pizza/ && STOP=true ./start-production-server.sh" || { echo "Failed to restart server"; exit 1; }
--- start-production-server.sh
+++ start-production-server.sh
@@ -1,9 +1,4 @@
-if [ "$REBUILD" = "true" ]; then
-  docker compose -f ./docker/docker-compose.yml \
-    -f ./docker/docker-compose-production.yml build
-  docker compose -f ./docker/docker-compose.yml \
-    -f ./docker/docker-compose-production.yml down
-fi
-
-docker compose -f ./docker/docker-compose.yml \
-  -f ./docker/docker-compose-production.yml up
+docker-compose -f ./docker/docker-compose.yml \
+  -f ./docker/docker-compose-production.yml down
+docker-compose -f ./docker/docker-compose.yml \
+  -f ./docker/docker-compose-production.yml up -d
