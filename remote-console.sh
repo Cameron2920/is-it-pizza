304c09eb90fdbb8a1f75611033cfdeca3c5eee3e
Cameron2920 <caramsay@uwaterloo.ca>
2022-10-15 03:01:54 -0400

    Added remote console.

---
+++ remote-console.sh
@@ -0,0 +1,24 @@
+if [ "$1" = "production" ]; then
+    ENV_FILE="app.env"
+    SSH_URL="root@178.128.135.203"
+    echo "Loading Production Environment."
+else
+    echo "Unknown Environment."
+    exit 1
+fi
+
+if test -z "$2"; then
+    RAILS_COMMAND="rails c"
+else
+    RAILS_COMMAND="$2"
+fi
+
+set -o allexport; source $ENV_FILE; set +o allexport
+export REMOTE_DATABASE_HOST=localhost
+export REMOTE_DATABASE_PORT=5099
+export DATABASE_HOST=localhost
+export DATABASE_PORT=$(( ((RANDOM<<15)|RANDOM) % 63001 + 2000 ))
+ssh -i ~/.ssh/server_keys/is-it-pizza -M -S ssh-socket -fnNT -L $DATABASE_PORT:$REMOTE_DATABASE_HOST:$REMOTE_DATABASE_PORT $SSH_URL
+spring stop
+eval $RAILS_COMMAND
+ssh -S ssh-socket -O exit $SSH_URL
--- start-production-server.sh
+++ start-production-server.sh
@@ -1,4 +1,4 @@
 docker-compose -f ./docker/docker-compose.yml \
-  -f ./docker/docker-compose-production.yml down
+  -f ./docker/docker-compose-production.yml stop app
 docker-compose -f ./docker/docker-compose.yml \
   -f ./docker/docker-compose-production.yml up -d
